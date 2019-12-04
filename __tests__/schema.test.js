c8bc2dad96ab65aae23a9dbfb778aabcc8b95d53
Casey Martell <drmartell@gmail.com>
2019-12-03 20:21:43 -0800

    Schema class complete with tests

--- __tests__/schema.test.js
+++ __tests__/schema.test.js
@@ -1,62 +1,44 @@
-const {
-  isNumber,
-  isString,
-  isBoolean,
-  isArray,
-  isObject,
-  getCaster,
-  castToNumber,
-  castToString,
-  castToBoolean,
-  castToArray,
-} = require('../lib/types.js');
-
-const Validator = require('../lib/Validator.js');
 const Schema = require('../lib/Schema.js');
 
 const schema = new Schema({
-  dog: {
+  name: {
     type: String,
     required: true
-  }, 
+  },
   age: {
     type: Number,
     required: true
   },
   weight: {
     type: String
-  }
+  },
 });
 
-const spot = {
+const dog1 = {
   name: 'spot',
   age: 5,
-  weight: '20 lbs'
+  weight: '20 lbs',
 };
 
-const rover = {
+const dog2 = {
   name: 'rover',
-  age: '10'
+  age: 10,
 };
 
-const who = {
-  age: 'hi'
+const dog3 = {
+  age: 10,
 };
 
-schema.validate(spot); // returns { name: 'spot', age: 5, weight: '20 lbs' }
-schema.validate(rover); // returns { name: 'rover', age: 10 };'
-schema.validate(who); // throws a errors about name being required and age not being a number
+const dog4 = {
+  name: 'rover',
+  age: 'hi',
+};
 
-const nameValidator1 = new Validator('name', { type: String, required: true });
-const nameValidator2 = new Validator('name', { type: String, required: false });
-
-describe('validator module', () => {
-  describe('validate method', () => {
-    it('returns a fields value when required', () => expect(nameValidator1.validate(dog1)).toEqual('spot'));
-    it('returns a fields value when not required', () => expect(nameValidator2.validate(dog1)).toEqual('spot'));
-    it('returns a fields value when required and needs casting', () => expect(nameValidator1.validate(dog2)).toEqual('55'));
-    it('returns a fields value when not required and needs casting', () => expect(nameValidator2.validate(dog2)).toEqual('55'));
-    it('throws an error when required and missing', () => expect(() => nameValidator1.validate(dog3)).toThrow());
-    it('throws an error when not required and missing', () => expect(() => nameValidator2.validate(dog3)).toThrow());
+describe('schema class module', () => {
+  describe('.validate method', () => {
+    it('returns original object when it includes unrequired field', () => expect(schema.validate(dog1)).toEqual(dog1));
+    it('returns original object when it excludes unrequired field', () => expect(schema.validate(dog2)).toEqual(dog2));
+    it('throws an error when a required field is missing', () => expect(() => schema.validate(dog3)).toThrow());
+    it('throws an error when a field is not of correct type', () => expect(() => schema.validate(dog4)).toThrow());
   });
 });
